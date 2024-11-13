69771a44c2c2a5d20396bb52278f255513fce87f
joy935 <tupaiv@gmail.com>
2024-11-13 13:20:28 +0100

    first commit

---
+++ swagger.js
@@ -0,0 +1,14 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+    info: {
+        title: "Temple API",
+        description: "API for temples",
+    },
+    host: "localhost:8080"
+};
+
+const outputFile = '/swagger.json';
+const routes = ['./index.js', './routes/temple.js'];
+
+swaggerAutogen(outputFile, routes, doc);
